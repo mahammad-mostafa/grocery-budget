b3bb032f1bd99e700d2952089c81ab19a274f031
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-10 00:12:31 +0200

    Built items controller with create & item_params methods

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,16 @@
+class ItemController < ApplicationController
+  def create
+    if @item.save
+      redirect_to(group_path(@item.groups.first))
+    else
+      flash.now[:errors] = @item.errors.full_messages
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :amount)
+  end
+end
