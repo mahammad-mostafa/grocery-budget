1d8f8eb4a5e1ac32ed7e01a4d613e64c26a05d58
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-10 20:35:16 +0200

    Included devise validation & registeration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :validatable
   validates :name, presence: true
   has_many :items, foreign_key: :author_id
   has_many :groups
