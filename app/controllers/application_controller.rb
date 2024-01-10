ee438405297bf857b8c42f75e1a3807f3fed6c2a
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-10 22:20:23 +0200

    Handled invalid permissions & not found routes or pathes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to(root_url, alert: 'Record not found')
+  end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to(root_url, alert: exception.message)
+  end
+
+  def index
+    redirect_to(root_url, alert: 'Invalid path')
+  end
 
   protected
 
