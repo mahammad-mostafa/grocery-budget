9b8fcfe5f22a246acca741422eb692796b0a51fc
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-13 00:25:10 +0200

    Implemented helper method for menu

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,24 @@
 class GroupsController < ApplicationController
   load_and_authorize_resource
+  def index
+    resource_menu('Group list')
+  end
+
+  def new
+    resource_menu('New group', groups_path, 'Save')
+  end
+
+  def show
+    session[:previous] = group_path(@group)
+    resource_menu('Group items', groups_path)
+  end
+
   def create
     if @group.save
       redirect_to(group_path(@group))
     else
       flash.now[:errors] = @group.errors.full_messages
+      resource_menu('New group', groups_path, 'Save')
       render(:new, status: :unprocessable_entity)
     end
   end
