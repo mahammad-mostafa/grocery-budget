b0f88aac7075ea6529074d89fa9129eaa138189d
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-13 00:24:00 +0200

    Skipped authentication for splash page & created helper method for menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound do
     redirect_to(root_url, alert: 'Record not found')
@@ -15,6 +15,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    resource_menu({ 'sessions' => 'Login', 'registrations' => 'Register' }, root_path, 'Submit')
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+  end
+
+  def resource_menu(title, previous = nil, action = nil)
+    @title = title
+    @previous = previous if previous
+    @action = action if action
   end
 end
