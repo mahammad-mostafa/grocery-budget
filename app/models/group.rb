48f752ea7133fd2161a58d922f4f77567cce4950
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-14 01:49:03 +0200

    Implemented total method for calculating total items amount

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :groups_items
   has_many :items, through: :groups_items
+  def total
+    items.reduce(0) { |total, element| total + element.amount }
+  end
 end
