74569fa29e4d085bbad8f3a253fc99bd5fb967e9
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-15 21:50:12 +0200

    Changed amount value for item to one

--- spec/setup_helper.rb
+++ spec/setup_helper.rb
@@ -2,7 +2,7 @@ module Setup
   def initializer(action = '')
     user = User.create!(name: 'user', email: 'email@domain.com', password: 'password')
     group = Group.create!(user_id: user.id, name: 'group', icon: 'https://placehold.co/100')
-    Item.create!(author_id: user.id, name: 'item', amount: 5, group_ids: [group.id])
+    Item.create!(author_id: user.id, name: 'item', amount: 1, group_ids: [group.id])
     sign_in(user) if action == 'login'
   end
 end
