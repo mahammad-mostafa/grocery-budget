08dabb284e1a1311f41377532615694e25f57f52
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-15 21:54:09 +0200

    Renamed join table migration file

--- db/migrate/20240109145024_create_join_table_group_item.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableGroupItem < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :groups, :items do |t|
-      t.index [:group_id, :item_id]
-      t.index [:item_id, :group_id]
-    end
-  end
-end
